c720a374005e4a45e07405db9800b962588b1709
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2019-11-13 19:42:32 -0300

    Reestructuracion de clase Detail.

--- proyecto/src/app/components/detail/detail.component.ts
+++ proyecto/src/app/components/detail/detail.component.ts
@@ -13,8 +13,12 @@ import { Analisis } from 'src/app/models/analisis';
 export class DetailComponent implements OnInit {
 
   private url: String;
-  private myAnalisis: Analisis=new Analisis('UsuarioPrueba',null,0.33,0.22,0.111,0.01,0.0124,0.212,0.4312,0.7832,0.0021,0.451);
-
+  private myAnalisis: Analisis;
+  public barChartOptions;
+  public barChartLabels;
+  public barChartType;
+  public barChartLegend; 
+  public barChartData;
 
   constructor(
     private _analisisService: AnalisisService,
@@ -26,8 +30,23 @@ export class DetailComponent implements OnInit {
     this._route.params.subscribe(params =>{
       let user =params.user;
        
-      //this.myAnalisis = new Analisis('UsuarioPrueba',null,0.33,0.22,0.111,0.01,0.0124,0.212,0.4312,0.7832,0.0021,0.451);
+      this.myAnalisis = new Analisis('UsuarioPrueba',null,0.33,0.22,0.111,0.01,0.0124,0.212,0.4312,0.7832,0.0021,0.451);
       //console.log(this.getAnalisis(user));
+      
+      this.barChartOptions = {
+        scaleShowVerticalLines: false,
+        responsive: true
+      };
+      this.barChartLabels = ['Anger', 'Disgust', 'Fear', 'Joy', 'Sadness'];
+      this.barChartType = 'bar';
+      this.barChartLegend = true;
+      this.barChartData = [
+        {data: [this.myAnalisis.getAnger(), this.myAnalisis.getDisgust(), this.myAnalisis.getFear(),this.myAnalisis.getJoy(), this.myAnalisis.getSadness()],
+         label: this.myAnalisis.getUserName(),
+         backgroundColor:['rgba(255, 102, 102, 1)','rgba(0,204,0, 1)','rgba(153, 102, 255, 1)','rgba(255, 204, 0, 1)','rgba(102, 204, 255 , 1)'],
+         hoverBackgroundColor:['rgba(255, 102, 102, 0.7)','rgba(0,204,0,0.7)','rgba(153, 102, 255, 0.7)','rgba(255, 204, 0, 0.7)','rgba(102, 204, 255 , 0.7)']
+        }
+      ];
     });
   }
 
@@ -41,23 +60,5 @@ export class DetailComponent implements OnInit {
       console.log(<any> error);
     });
   }
-
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-
-  public barChartLabels = ['Anger', 'Disgust', 'Fear', 'Joy', 'Sadness'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-
-  public barChartData = [
-    {data: [this.myAnalisis.getAnger(), this.myAnalisis.getDisgust(), this.myAnalisis.getFear(),this.myAnalisis.getJoy(), this.myAnalisis.getSadness()],
-     label: this.myAnalisis.getUserName(),
-     backgroundColor:['rgba(255, 102, 102, 1)','rgba(0,204,0, 1)','rgba(153, 102, 255, 1)','rgba(255, 204, 0, 1)','rgba(102, 204, 255 , 1)'],
-     hoverBackgroundColor:['rgba(255, 102, 102, 0.7)','rgba(0,204,0,0.7)','rgba(153, 102, 255, 0.7)','rgba(255, 204, 0, 0.7)','rgba(102, 204, 255 , 0.7)']
-    }
-  ];
-
 
 }
