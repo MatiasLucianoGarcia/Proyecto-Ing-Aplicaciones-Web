60546cc46dacc2d69771f64613459fcd40e79a82
MatiasLucianoGarcia <mati.luciano.garcia@gmail.com>
2019-11-18 22:45:36 -0300

    Agregado Radar Chart en funcionamiento.

--- proyecto/src/app/components/detail/detail.component.ts
+++ proyecto/src/app/components/detail/detail.component.ts
@@ -14,12 +14,19 @@ export class DetailComponent implements OnInit {
 
   private url: String;
   private myAnalisis: Analisis;
+  private analisisJson;
+
+  //Atributos de BarChart
   public barChartOptions;
   public barChartLabels;
   public barChartType;
   public barChartLegend; 
   public barChartData;
-  private analisisJson;
+
+  //Atributos de RadarChart
+  public radarChartLabels;
+  public radarChartType;
+  public radarChartData;
 
   constructor(
     private _analisisService: AnalisisService,
@@ -30,11 +37,12 @@ export class DetailComponent implements OnInit {
   ngOnInit() {
     this._route.params.subscribe(async params =>{
       let user =params.user;
-      this.setConfiguration(user);
+      //this.setConfiguration(user);
       
       //Codigo de Test 
-      //this.myAnalisis = new Analisis('UsuarioPrueba',"http://pbs.twimg.com/profile_images/668940727800471552/n5j2l2Dm_normal.jpg",0.33,0.22,0.111,0.01,0.0124,0.212,0.4312,0.7832,0.0021,0.451);
-      //this.setBarChart();
+      this.myAnalisis = new Analisis('UsuarioPrueba',"http://pbs.twimg.com/profile_images/668940727800471552/n5j2l2Dm_normal.jpg",0.33,0.22,0.111,0.01,0.0124,0.212,0.4312,0.7832,0.0021,0.451);
+      this.setBarChart();
+      this.setRadarChart();
       
     });
   }
@@ -56,6 +64,7 @@ export class DetailComponent implements OnInit {
           this.analisisJson.agreeableness,
           this.analisisJson.emotionalRange);
           this.setBarChart();
+          this.setRadarChart();
     },
     error=>{
       console.log(<any> error);
@@ -75,6 +84,17 @@ export class DetailComponent implements OnInit {
        label: this.myAnalisis.getUserName(),
        backgroundColor:['rgba(255, 102, 102, 1)','rgba(0,204,0, 1)','rgba(153, 102, 255, 1)','rgba(255, 204, 0, 1)','rgba(102, 204, 255 , 1)'],
        hoverBackgroundColor:['rgba(255, 102, 102, 0.7)','rgba(0,204,0,0.7)','rgba(153, 102, 255, 0.7)','rgba(255, 204, 0, 0.7)','rgba(102, 204, 255 , 0.7)']
+      }
+    ];
+  }
+
+  setRadarChart(){
+    this.radarChartLabels = ['Openness', 'Conscientiousness', 'Extraversion', 'Agreeableness', 'EmotionalRange'];
+    this.radarChartType = 'radar';
+    this.radarChartData = [
+      {
+        data: [this.myAnalisis.getOpenness(), this.myAnalisis.getConscientiousness(), this.myAnalisis.getExtraversion(),this.myAnalisis.getAgreeableness(),this.myAnalisis.getEmotionalRange()],
+        label: this.myAnalisis.getUserName()
       }
     ];
   }
